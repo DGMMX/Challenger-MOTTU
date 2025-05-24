6a9babe9d056d76e43ae872745f915b351f0ed41
DGMMX <dgmmxs2@outlook.com>
2025-05-24 14:01:32 -0300

    .

--- Challenger-MOTTU/Program.cs
+++ Challenger-MOTTU/Program.cs
@@ -1,5 +1,11 @@
 
-namespace Challenger_MOTTU
+using challenger.Domain.Entities;
+using challenger.Infrastructure.Context;
+using challenger.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace challenger
 {
     public class Program
     {
@@ -9,16 +15,44 @@ namespace Challenger_MOTTU
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddControllers()
+                .AddJsonOptions(x =>
+                    x.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve
+                );
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = builder.Configuration["Swagger:Title"],
+                    Description = builder.Configuration["Swagger:Description"] + DateTime.Now.Year,
+                    Contact = new OpenApiContact()
+                    {
+                        Email = "rm558710@fiap.com.br",
+                        Name = "Diego Bassalo"
+                    }
+                });
+            });
+
+            builder.Services.AddDbContext<CGContext>(options =>
+            {
+                options.UseOracle(builder.Configuration.GetConnectionString("Oracle"));
+            });
+
+            builder.Services.AddScoped<IRepository<Moto>, Repository<Moto>>(); // genérico, opcional se usar específico
+            builder.Services.AddScoped<IMotoRepository, MotoRepository>();
+
+            builder.Services.AddScoped<IRepository<Patio>, Repository<Patio>>();
+            builder.Services.AddScoped<IPatioRepository, PatioRepository>();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
